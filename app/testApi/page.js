99aba985dd88c0f8085cbc2dd81523a124190305
JaRoD41 <laurent.trinco@gmail.com>
2023-09-21 15:50:16 +0200

    add testApi and axios

--- app/testApi/page.js
+++ app/testApi/page.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+'use client'
+import { useState, useEffect, useLayoutEffect } from 'react'
 import axios from 'axios'
 // https://dumbstockapi.com/stock?exchanges=NYSE
 
@@ -6,20 +7,30 @@ const testApi = () => {
 	const [data, setData] = useState(null)
 	const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get('https://dumbstockapi.com/stock?exchanges=NYSE')
-      setData(response.data)
-      setLoading(false)
-    }
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get('https://dumbstockapi.com/stock?exchanges=NYSE')
+				setData(response.data)
+				setLoading(false)
+			} catch (error) {
+				console.log(error)
+				setLoading(false)
+			}
+		}
 
-    fetchData()
+		fetchData()
 
-    return () => {
-      console.log('cleanup')
-    }
-  }), []
-	return <div>testApi</div>
+		return () => {
+			// console.log('cleanup')
+		}
+	}),
+		[]
+	return (
+		<div>
+			{loading ? <p>Loading...</p> : <ul>{data && data.map((item) => <li key={item.ticker}>{item.name}</li>)}</ul>}
+		</div>
+	)
 }
 
 export default testApi
